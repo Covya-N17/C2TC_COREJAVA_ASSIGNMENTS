280f8daa4c175e8e65263ef3aa97cc87ea00cbbb
Covya-N17 <covyanadarajan2004@gmail.com>
2025-08-29 09:09:18 +0530

    commit

---
+++ C2TC_CoreJava_Assignments/src/com/tnsif/assignmentsix/InsufficientFundsException.java
@@ -0,0 +1,8 @@
+package com.tnsif.assignmentsix;
+
+//InsufficientFundsException.java
+public class InsufficientFundsException extends Exception {
+public InsufficientFundsException(String message) {
+   super(message);
+}
+}
